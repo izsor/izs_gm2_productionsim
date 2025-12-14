43012814b67ecda21b0633d684f761d367c4640d
izsor <izso.roland@gmail.com>
2025-12-15 00:21:37 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
     name=package_name,
     version='0.1.0',
     packages=find_packages(exclude=['test']),
+    package_name = 'izs_gm2_productionsim'
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Roland Izsó',
