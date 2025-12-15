cf6f6dfaea0e7ae4a3374d27896869f589e2ab52
izsor <izso.roland@gmail.com>
2025-12-15 01:08:40 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,31 @@
-import os
+from setuptools import find_packages, setup
 from glob import glob
-from setuptools import setup
+import os
 
 package_name = 'izs_gm2_productionsim'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=[package_name],
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='izsor',
-    maintainer_email='TODO',
-    description='TODO',
-    license='GPL-3.0',
+    maintainer='Roland Izsó',
+    maintainer_email='izso.roland@gmail.com',
+    description='ROS 2 manufacturing line simulation (line -> measure -> quality -> stats).',
+    license='GNU General Public License v3.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'workpiece_generator = izs_gm2_productionsim.workpiece_generator:main',
-            'measurement_station = izs_gm2_productionsim.measurement_station:main',
-            'quality_inspector = izs_gm2_productionsim.quality_inspector:main',
+            'line = izs_gm2_productionsim.line_node:main',
+            'device = izs_gm2_productionsim.measure_node:main',
+            'quality = izs_gm2_productionsim.quality_node:main',
+            'stats = izs_gm2_productionsim.stats_node:main',
         ],
     },
 )
