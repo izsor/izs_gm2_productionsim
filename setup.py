8ac9ef342dc52c65fbe1a872531f0a97d078b24d
izsor <izso.roland@gmail.com>
2025-12-15 01:43:13 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -23,11 +23,12 @@ setup(
     entry_points={
     'console_scripts': [
         'line = izs_gm2_productionsim.line_node:main',
-        'measurement = izs_gm2_productionsim.measure_node:main',
-        'inspection = izs_gm2_productionsim.quality_node:main',
+        'device = izs_gm2_productionsim.measure_node:main',
+        'quality = izs_gm2_productionsim.quality_node:main',
         'stats = izs_gm2_productionsim.stats_node:main',
         'viz = izs_gm2_productionsim.viz_node:main',
     ],
-},
+    },
+
 
 )
